45e1fce4e20ee34a7850770f036b2357d13619e7
GRASSYAU <kj2940858@gmail.com>
2020-10-17 22:23:32 +0800

    2020/10/17

---
+++ my-first-app/pages/counter.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>
+        Counter
+        <!-- 用冒號來綁定class來條件判斷 :class="[{ }]"  -->
+        <!-- 如果num > 5這個span才會擁有 text-red 這個class -->
+        <span :class="[ { 'text-red': num >5 } ]">{{ num }}</span>
+        <!-- <span :class="[ { 'text-red': num %2 ===0 } ]">{{ num }}</span> 求偶數 除2=0 -->
+      </h1>
+      <!-- v-if是條件判斷達成才會出現此標籤 -->
+      <h4 v-if="num > 5">現在這個num已經超過5了!</h4>
+      <!-- TODO: 設計計數器UI -->
+      <button @click="minus">-</button>
+      <button @click="add">+</button>
+      <button v-if="num!==0" @click="resetNum">歸零</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "counterPage",
+  data(){
+    return{
+      //這個vue元件有哪些資料
+      num:0,
+    }
+  },
+  methods:{
+    //這個vue元件可用的方法(函數,功能)
+    add(){
+      console.log(`按下了標籤屬性`)
+      //把num+1
+      //把vue元件(this)內要取得元件的資料
+      //this,資料名稱
+      //tihs這裡指vue元件
+      console.log(this.num);
+      //使用this指定物件名寫入要呈現的功能，在塞回原本的物件來改變內容 類似js寫變數
+      //this.num = this.num+1;的縮寫
+      //如果num >=10
+      if(this.num >=10){
+        //讓函數停止
+        return;
+      }
+      this.num += 1;
+    },
+    minus(){
+      //num如果大於0才會-1
+      if(this.num > 0){
+        this.num -= 1;
+      }
+    },
+    resetNum(){
+      if (this.num >0) {
+        this.num = 0;
+      }
+    }
+  }
+};
+</script>
---
+++ my-first-app/pages/todo-list.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>TODO List</h1>
+      <!-- TODO: 設計新增待辦事項UI -->
+      <div class="create-todo-ui"></div>
+      <!-- 當標單送出時(不會重整)執行createTodo這個methods -->
+        <form @submit.prevent="createTodo">
+          <input v-model="todotext" type="text" required placeholder="請輸入待辦內容">
+          <select name="" id="" required v-model="todoCategory">
+            <option value="" disabled>請選擇類別</option>
+            <option value="財務">財務</option>
+            <option value="健康">健康</option>
+            <option value="學習">學習</option>
+          </select>
+          <button>新增</button>
+        </form>
+        <!-- <h3>準備新增待辦事項:{{ todotext }}</h3> -->
+      <!-- TODO: 待辦事項列表 -->
+      <!-- <p>{{ todoList }}</p> -->
+      <div class="todo-list-ui">
+        <!-- 每一個待辦事項的模板 -->
+        <div v-for="todo in todoList" :key="todo.createdAt" class="todo-item">
+          <div class="toto-category">{{ todo.category }}</div>
+          <div class="todo-text">{{ todo.text }}</div>
+        </div>
+        <!-- 每一個待辦事項的模板 end -->
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "todoListPage",
+  data: ()=>{
+    return{
+      todotext:"",
+      todoCategory:"",
+      //待辦清單
+      todoList:[]
+    }
+  },
+  methods:{
+    createTodo(){
+      console.log("[新增待辦表單送出了]")
+      //TODO:描述一份待辦事項的資料(內容，分類，建立時間)
+      const todo = {
+        text:this.todotext,
+        category:this.todoCategory,
+        createdAt: new Date().getTime() //時間
+      };
+      console.log("[新待辦]", todo)
+      //把(todo)待辦事項新增(push())到待辦清單(todoList)裡面
+      //[].push('資料1') => ['資料1']
+      this.todoList.push(todo)
+    }
+  }
+};
+</script>
